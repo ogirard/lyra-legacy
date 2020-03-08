7282bf8114fcb05d38ba32d56ea40f7b01b0ae3e
Olivier Girard <oli.girard@gmail.com>
2020-03-08 22:09:10 +0100

    Create versioned release

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -1,7 +1,10 @@
-if (Test-Path Lyra-Release.zip) { 
-    Remove-Item Lyra-Release.zip --Force -ErrorAction Ignore
+param(
+    [String] $ReleaseName = "Lyra-Release.zip"
+)
+if (Test-Path $ReleaseName) { 
+    Remove-Item $ReleaseName -ErrorAction Ignore
 }
 
 Remove-Item .\LyraShell\bin\Release\store\ -Force -Recurse -ErrorAction Ignore
 
-Compress-Archive -Path .\LyraShell\bin\Release -DestinationPath Lyra-Release.zip
+Compress-Archive -Path .\LyraShell\bin\Release -DestinationPath .\$ReleaseName
