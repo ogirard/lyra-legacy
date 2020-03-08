47e52a3f0c6f840dcbfd3d2cd6bc0a5224354656
Olivier Girard <oli.girard@gmail.com>
2020-03-08 21:58:49 +0100

    Update Create-Release.ps1

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -1,2 +1,7 @@
+if (Test-Path Lyra-Release.zip) { 
+    Remove-Item Lyra-Release.zip --Force -ErrorAction Ignore
+}
+
 Remove-Item .\LyraShell\bin\Release\store\ -Force -Recurse -ErrorAction Ignore
+
 Compress-Archive -Path .\LyraShell\bin\Release -DestinationPath Lyra-Release.zip
